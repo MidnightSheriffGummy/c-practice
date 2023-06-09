6d3a3102cf54b714c9789b5f8d22720f709719d2
MidnightSheriffGummy <yuna2111@naver.com>
2023-06-09 21:53:43 +0900

    scanf 함수 연습

--- hello/sum.c
+++ hello/sum.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-void add(void) {
+void dd(void) {
 	int x;
 	int y;
 	int sum;
@@ -32,9 +32,68 @@ void exchangeRate(void)
 	printf("원화 %d원은 %lf달러 입니다.\n", krw, usd);
 }
 
+void calculate(void)
+{
+	double x;
+	printf("실수를 입력하세요: ");
+	scanf_s("%lf", &x);
+
+	double result = 3 * x * x + 7 * x + 11;
+
+		printf("다항식의 값%lf:", result);
+
+}
+
+void weightOfMoon(void)
+{
+	double x;
+	printf("몸무게를 입력하세요: ");
+	scanf_s("%lf", &x);
+	printf("달에서의 몸무게:%lf", x * 17 / 100);
+	
+}
+
+void average(void)
+{
+	double x, y, z;
+
+	printf("실수를 입력하세요:");
+	scanf_s("%lf", &x);
+
+	printf("실수를 입력하세요:");
+	scanf_s("%lf", &y);
+
+	printf("실수를 입력하세요:");
+	scanf_s("%lf", &z);
+
+
+	double sum = x + y + z;
+	double avg = sum / 3;
+
+	printf("합은 %lf이고 평균은 %lf입니다",sum ,avg );
+
+}
+
+void add(void)
+{
+	int a, b;
+	scanf_s("%d", &a);
+	scanf_s("%d", &b);
+	printf("%d", a + b);
+	
+}
+
+void diff(void)
+{
+	int a, b;
+	scanf_s("%d", &a);
+	scanf_s("%d", &b);
+	printf("%d", a - b);
+}
+
 int main(void)
 {
-	exchangeRate();
+	diff();
 	return 0;
 
 }
